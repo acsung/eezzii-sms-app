3c25eb8db86bf536785b426975464966d38bc92c
acsung <145598506+acsung@users.noreply.github.com>
2025-06-17 08:10:44 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  'https://xawgyywwsykfncoskjjp.supabase.co',
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
+);
+
 
 export default function Scheduled() {
   const [scheduledMessages, setScheduledMessages] = useState([])
